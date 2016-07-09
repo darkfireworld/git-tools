0cf6bfd6583cfff273d52995737ad6b76dd709df
暗火世界 <darkfireworld@qq.com>
2016-07-09 13:26:19 +0800

    gitkeep->keep & fetch -> check [fetch]

--- src/main/java/org/darkfireworld/GitTools.java
+++ src/main/java/org/darkfireworld/GitTools.java
@@ -1,13 +1,9 @@
 package org.darkfireworld;
 
-import com.google.common.base.Joiner;
-import com.google.common.base.Strings;
-import com.google.common.io.ByteStreams;
-import com.google.common.io.Files;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -55,7 +51,7 @@ public class GitTools {
         //git keep
         {
             GitTools.getInstance().register(new Cmd() {
-                final String CMD_NAME = "gitkeep";
+                final String CMD_NAME = "keep";
                 final String KEEP_FILE_NAME = ".gitkeep";
 
                 @Override
@@ -109,13 +105,11 @@ public class GitTools {
                 }
             });
         }
-        //git fetch
+        //git check
         {
             GitTools.getInstance().register(new Cmd() {
                 //指令关键字
-                final String CMD_NAME = "fetch";
-                //更新关键字
-                final String KEY_REGEX = "[\\s\\S]*POST[\\s\\S]*";
+                final String CMD_NAME = "check";
                 //分隔符
                 final String SEP_LINE = "-----------------------------------------------------";
 
@@ -126,33 +120,53 @@ public class GitTools {
 
                 @Override
                 public void todo(String[] option) {
-                    //执行结果信息, value->Boolean(true 存在更新，false 不存在更新) | Exception(执行异常)
+                    //是否进行fetch操作
+                    boolean fetch = option.length > 0 && "fetch".equalsIgnoreCase(option[0]);
                     Map<String, Object> executeMap = new LinkedHashMap<String, Object>();
-                    //遍历当前目录下，所有的子目录，如果该子目录为git仓库(包含.git/)，则同步
-                    for (File file : (new File(".")).listFiles()) {
-                        if (file.isDirectory()) {
-                            //检测是否存在.git目录
-                            File childGitDir = new File(file, ".git");
-                            if (childGitDir.exists() && childGitDir.isDirectory()) {
-                                //同步
-                                try {
-                                    boolean res = fetch(childGitDir);
-                                    executeMap.put(file.getName(), res);
-                                } catch (Throwable e) {
-                                    //打印异常
-                                    e.printStackTrace();
-                                    executeMap.put(file.getName(), e);
+                    //需要检测的目录
+                    List<File> repoDirList = new LinkedList<File>();
+                    {
+                        boolean filled = false;
+                        //检测当前目录是否就是一个仓库
+                        {
+                            File file = new File(".git");
+                            if (file.exists() && file.isDirectory()) {
+                                repoDirList.add(new File("."));
+                                filled = true;
+                            }
+                        }
+                        //读取当前目录下所有的目录，整理出目录中带有.git的子目录
+                        if (!filled) {
+                            for (File file : new File(".").listFiles()) {
+                                if (file.isDirectory()) {
+                                    //检测是否存在.git目录
+                                    File childGitDir = new File(file, ".git");
+                                    if (childGitDir.exists() && childGitDir.isDirectory()) {
+                                        repoDirList.add(file);
+                                    }
                                 }
                             }
                         }
                     }
+                    //遍历当前目录下，所有的子目录，如果该子目录为git仓库(包含.git/)，则同步
+                    for (File file : repoDirList) {
+                        //同步
+                        try {
+                            boolean res = check(file, fetch);
+                            executeMap.put(file.getName(), res);
+                        } catch (Throwable e) {
+                            //打印异常
+                            e.printStackTrace();
+                            executeMap.put(file.getName(), e);
+                        }
+                    }
                     //分隔符
                     System.out.println(SEP_LINE);
-                    System.out.println("FETCH信息：");
+                    System.out.println("CHECK信息：");
                     System.out.println();
                     //状态计数
-                    int updateCount = 0;
-                    int sameCount = 0;
+                    int dirtyCount = 0;
+                    int cleanCount = 0;
                     int errorCount = 0;
                     //打印报告
                     for (String key : executeMap.keySet()) {
@@ -161,11 +175,11 @@ public class GitTools {
                         if (val instanceof Boolean) {
                             boolean res = (Boolean) val;
                             if (res) {
-                                updateCount++;
-                                msg = "UPDATE";
+                                cleanCount++;
+                                msg = "CLEAN";
                             } else {
-                                sameCount++;
-                                msg = "SAME";
+                                dirtyCount++;
+                                msg = "DIRTY";
                             }
                         } else {
                             errorCount++;
@@ -175,13 +189,13 @@ public class GitTools {
                     }
                     //打印统计信息
                     System.out.println(SEP_LINE);
-                    System.out.println("统计信息：");
+                    System.out.println("COUNT信息：");
                     System.out.println();
 
-                    //需要更新的仓库
-                    System.out.println(String.format("UPDATE COUNT：%s", updateCount));
-                    //没有更新的仓库
-                    System.out.println(String.format("SAME COUNT：%s", sameCount));
+                    //干净的仓库
+                    System.out.println(String.format("CLEAN COUNT：%s", cleanCount));
+                    //dirty仓库
+                    System.out.println(String.format("DIRTY COUNT：%s", dirtyCount));
                     //更新错误的仓库
                     System.out.println(String.format("ERROR COUNT：%s", errorCount));
                     //仓库总数
@@ -191,50 +205,177 @@ public class GitTools {
                 /**
                  * 为当前目录，执行 fetch -vp --all 指令，如果包含 "POST" 关键字，则返回true
                  *
-                 * @param dir 指向.git的路径
-                 * @return true 同步的时候发现关键字
+                 * @param repoDir   包含.git的目录
+                 * @param fetch 是否进行fetch 操作
+                 * @return true 检测通过，false检测失败
+                 */
+                boolean check(File repoDir, boolean fetch) throws Exception {
+                    //print repo name
+                    {
+                        System.out.println(SEP_LINE);
+                        System.out.println();
+                        System.out.println(String.format("REPO：%s", repoDir.getName()));
+                        System.out.println();
+                    }
+                    //fetch
+                    {
+                        if (fetch) {
+                            call(repoDir, "FETCH", new String[]{"git", "fetch", "-vp", "--all"});
+                        }
+                    }
+                    //检测status
+                    {
+                        //status
+                        String log = call(repoDir, "STATUS", new String[]{"git", "status"});
+                        boolean isClean = log.matches("[\\s\\S]*working directory clean[\\s\\S]*");
+                        //如果不干净，则直接返回
+                        if (!isClean) {
+                            return false;
+                        }
+                    }
+                    //git branch check
+                    {
+                        //引用
+                        class Ref {
+                            String branchName;
+                            String repoName;
+                            String objectId;
+
+                            public Ref(String branchName, String repoName, String objectId) {
+                                this.branchName = branchName;
+                                this.repoName = repoName;
+                                this.objectId = objectId;
+                            }
+
+                            public String getBranchName() {
+                                return branchName;
+                            }
+
+                            public String getRepoName() {
+                                return repoName;
+                            }
+
+                            public String getObjectId() {
+                                return objectId;
+                            }
+                        }
+
+                        //远程仓库
+                        List<String> remotes = new ArrayList<String>();
+                        //引用分支->objectId
+
+                        Map<String, List<Ref>> refsMap = new HashMap<String, List<Ref>>();
+                        //读取远程仓库数量
+                        {
+                            String log = call(repoDir, "REMOTE", new String[]{"git", "remote"});
+                            for (String remote : log.split("\\n")) {
+                                remotes.add(remote);
+                            }
+                        }
+                        //整合map
+                        {
+                            String log = call(repoDir, "SHOW-REF", new String[]{"git", "show-ref"});
+                            for (String line : log.split("\\n")) {
+                                String[] segment = line.split("\\s");
+                                String objectId = segment[0];
+                                // /refs/heads/master | /refs/remote/origin/HEAD | /refs/remote/origin/master
+                                String[] ref = segment[1].split("/");
+                                String refRepoName = ref[ref.length - 2];
+                                String refBranchName = ref[ref.length - 1];
+                                //跳过标签
+                                if ("tag".equalsIgnoreCase(refRepoName)) {
+                                    continue;
+                                }
+                                //跳过特殊branch
+                                if ("HEAD".equalsIgnoreCase(refBranchName)) {
+                                    continue;
+                                }
+                                List<Ref> refs = refsMap.get(refBranchName);
+                                if (refs == null) {
+                                    refs = new ArrayList<Ref>();
+                                    refsMap.put(refBranchName, refs);
+                                }
+                                refs.add(new Ref(refBranchName, refRepoName, objectId));
+                            }
+                        }
+                        //check branch dirty
+                        {
+                            for (String refBranchName : refsMap.keySet()) {
+                                List<Ref> refs = refsMap.get(refBranchName);
+                                //检测该分支上所有的objectId是否一致
+                                for (Ref ref : refs) {
+                                    //存在不相等的objectId 分支
+                                    if (!ref.getObjectId().equals(refs.get(0).getObjectId())) {
+                                        return false;
+                                    }
+                                }
+                                //检测，如果该分支的本地分支存在，则强制所有远程分支都需要具有该分支
+                                boolean isLocalExists = false;
+                                for (Ref ref : refs) {
+                                    //检测是否存在本地分支
+                                    if ("heads".equalsIgnoreCase(ref.getRepoName())) {
+                                        isLocalExists = true;
+                                        break;
+                                    }
+                                }
+                                //如果不相等，则则表示分支总和错误
+                                if (refs.size() != (remotes.size() + (isLocalExists ? 1 : 0))) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                    //检测通过
+                    return true;
+                }
+
+                /**
+                 * 运行一个指令
+                 * @param workDir 工作目录
+                 * @param what 指令名称
+                 * @param cmd 命令
+                 * @return 返回执行日志
                  * */
-                boolean fetch(File dir) throws Exception {
-                    //打印目录
-                    ProcessBuilder processBuilder = new ProcessBuilder("git", String.format("--git-dir=%s", dir.getCanonicalPath()), "fetch", "-vp", "--all");
-                    //打印执行日志
-                    System.out.println(SEP_LINE);
-                    System.out.println(String.format("CMD：%s", Joiner.on(" ").join(processBuilder.command())));
+                String call(File workDir, String what, String[] cmd) throws Exception {
+                    //status
+                    ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+                    //设置工作目录
+                    processBuilder.directory(workDir);
+                    System.out.println(String.format("%s ：%s", what, String.join(" ", processBuilder.command())));
                     //开始执行
                     Process process = processBuilder.start();
                     //等待执行完毕
                     int retCode = process.waitFor();
 
-                    //执行日志
-                    StringBuilder sb = new StringBuilder();
-                    //读取正常信息流
-                    ByteArrayOutputStream normalBos = new ByteArrayOutputStream();
-                    ByteStreams.copy(process.getInputStream(), normalBos);
-                    //转换称为文本
-                    sb.append(new String(normalBos.toByteArray()));
-
-                    //读取错误日志
-                    ByteArrayOutputStream errorBos = new ByteArrayOutputStream();
-                    ByteStreams.copy(process.getErrorStream(), errorBos);
-                    sb.append(new String(errorBos.toByteArray()));
-
-                    String log = sb.toString();
+                    //输出结果
+                    String log;
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    InputStream outIs = process.getInputStream();
+                    InputStream errIs = process.getErrorStream();
+                    try {
+                        IoKit.copyStream(outIs, bos, null);
+                        IoKit.copyStream(errIs, bos, null);
+                        //生成执行日志
+                        log = new String(bos.toByteArray());
+                    } finally {
+                        IoKit.closeSilently(outIs);
+                        IoKit.closeSilently(errIs);
+                    }
                     //打印执行日志
                     System.out.println();
                     System.out.println(log);
                     //执行失败
                     if (retCode != 0) {
-                        throw new Exception(String.format("git 执行异常：%s", log));
-                    } else {
-                        //返回正则匹配关键字信息
-                        return log.matches(KEY_REGEX);
+                        throw new Exception(String.format("%s 执行异常：%s", what, log));
                     }
+                    return log;
                 }
 
                 @Override
                 public String help() {
-                    return String.format("%s -> 遍历当前目录下所有子目录，为git的仓库执行：fetch -vp --all 命令", CMD_NAME);
+                    return String.format("%s [fetch]-> 遍历当前目录下所有子目录，检测是否存在dirty的git仓库，fetch会进行同步远程分支", CMD_NAME);
                 }
+
             });
         }
         //执行
